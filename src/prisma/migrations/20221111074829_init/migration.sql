ebc4843e7473ef3c7463460e28d8ada065a625b9
unknownpgr <unknownpgr@gmail.com>
2022-11-11 07:49:40 +0000

    Add timestamp on tables

--- src/prisma/migrations/20221111074829_init/migration.sql
+++ src/prisma/migrations/20221111074829_init/migration.sql
@@ -1,13 +1,17 @@
 -- CreateTable
 CREATE TABLE "Memo" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "content" TEXT NOT NULL
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Tag" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "value" TEXT NOT NULL
+    "value" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
@@ -17,6 +21,9 @@ CREATE TABLE "_MemoToTag" (
     CONSTRAINT "_MemoToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Memo" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "_MemoToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_value_key" ON "Tag"("value");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_MemoToTag_AB_unique" ON "_MemoToTag"("A", "B");

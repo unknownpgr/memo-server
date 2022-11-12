706388ac17bacade761cef87b3fc8c107e349929
unknownpgr <unknownpgr@gmail.com>
2022-11-12 14:39:25 +0900

    Update comment for middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,7 +16,6 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
      * - _next/static (static files)
      * - favicon.ico (favicon file)
      */

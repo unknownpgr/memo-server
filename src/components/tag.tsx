109a5a745fa7cd963aab8854c4d87993a70659e8
unknownpgr <unknownpgr@gmail.com>
2022-11-12 16:24:17 +0900

    design update

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -4,8 +4,8 @@ import styles from "../styles/tag.module.css";
 
 export default function tag({ value }: { value: string }) {
   return (
-    <Link href={`/tag/${value}`}>
-      <span className={styles.tag}>#{value}</span>
+    <Link className={styles.tag} href={`/tag/${value}`}>
+      <span>#{value}</span>
     </Link>
   );
 }

cd7580b713b07c101b1c82b5ca323e2182daeb8e
unknownpgr <unknownpgr@gmail.com>
2022-11-12 14:44:13 +0900

    Add redirection on login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 
+import { useRouter } from "next/router";
+
 export default function Login() {
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   async function login() {
-    await fetch("/api/login", {
+    const response = await fetch("/api/login", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ password }),
     });
+    if (response.status === 200) router.push("/");
+    else setPassword("");
   }
 
   return (

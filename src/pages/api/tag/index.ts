d27743ea7a058e80f33f73601efcea6017aa10ff
unknownpgr <unknownpgr@gmail.com>
2022-11-12 14:35:38 +0900

    Remove meaningless comments

--- src/pages/api/tag/index.ts
+++ src/pages/api/tag/index.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient, Tag } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 

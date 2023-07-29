7bd0562f00825a5d78e6610f015c1721b1acaaed
unknownpgr <unknownpgr@gmail.com>
2023-07-29 10:08:15 +0000

    Implement autosaving

--- src/pages/memo/[number].tsx
+++ src/pages/memo/[number].tsx
@@ -1,7 +1,7 @@
 import { onGetMemo, onUpdateMemo } from "../../telefunc/index.telefunc";
 
 import { InferGetServerSidePropsType } from "next";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Tags from "../../components/TagSelector";
 import { IMemo } from "../../global";
 import { findMemo } from "../../logic/logic";
@@ -37,52 +37,34 @@ export default function View({
   initialMemo,
   number,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isChanged, setIsChanged] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
   const [content, setContent] = useState(initialMemo?.content || "");
   const [tags, setTags] = useState<string[]>(
     initialMemo?.tags.map((x) => x.value) || []
   );
+  const debouncingRef = useRef<NodeJS.Timeout | null>(null);
 
-  async function refresh() {
-    const memo = await onGetMemo(int(number));
-    if (!memo) return;
-    setContent(memo.content);
-    setTags(memo.tags.map((x) => x.value));
-    setIsLoading(false);
-  }
-
-  async function updateMemo() {
-    setIsLoading(true);
-    await onUpdateMemo(content, tags, int(number));
-    await refresh();
-    setIsChanged(false);
-  }
+  useEffect(() => {
+    setIsSaving(true);
+    const timeout = setTimeout(async () => {
+      if (debouncingRef.current !== timeout) return;
+      await onUpdateMemo(content, tags, int(number));
+      setIsSaving(false);
+    }, 1000);
+    debouncingRef.current = timeout;
+  }, [number, content, tags]);
 
   return (
     <div>
       <h1>
-        Memo #{number}{" "}
-        <button onClick={updateMemo} disabled={!isChanged}>
-          [ update ]
-        </button>
+        Memo #{number} {isSaving && "Saving"}
       </h1>
       <textarea
         className={memoStyles.content}
-        disabled={isLoading}
         value={content}
-        onChange={(e) => {
-          setContent(e.target.value);
-          if (!isChanged) setIsChanged(true);
-        }}
+        onChange={(e) => setContent(e.target.value)}
       ></textarea>
-      <Tags
-        tags={tags}
-        setTags={(v) => {
-          setTags(v);
-          if (!isChanged) setIsChanged(true);
-        }}
-      ></Tags>
+      <Tags tags={tags} setTags={(v) => setTags(v)}></Tags>
     </div>
   );
 }

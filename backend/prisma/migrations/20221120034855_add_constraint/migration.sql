b0fad5e475e9ce11cb614558a602505b461e8100
unknownpgr <unknownpgr@gmail.com>
2023-09-30 13:15:01 +0000

    Update gitignore

--- backend/prisma/migrations/20221120034855_add_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[number,userId]` on the table `Memo` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Memo_number_userId_key" ON "Memo"("number", "userId");

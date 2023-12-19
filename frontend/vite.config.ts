f0365f59ad9b720f55d08af0f93e69422138593e
unknownpgr <unknownpgr@gmail.com>
2023-12-19 07:55:09 +0000

    Config dev env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     proxy: {
-      "/api": "http://localhost",
+      "/api": "http://memo-back",
     },
   },
 });

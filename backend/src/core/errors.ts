4e2393e52d66b50e0c8b0e3a7393530106151e0d
unknownpgr <unknownpgr@gmail.com>
2025-07-09 21:51:48 +0000

    Update custom errors to inherit `CustomError` superclass

--- backend/src/core/errors.ts
+++ backend/src/core/errors.ts
@@ -1,39 +1,41 @@
-export class UnauthorizedError extends Error {
+export class CustomError extends Error {}
+
+export class UnauthorizedError extends CustomError {
   constructor(message: string = "Unauthorized") {
     super(message);
     this.name = "UnauthorizedError";
   }
 }
 
-export class ServiceUninitializedError extends Error {
+export class ServiceUninitializedError extends CustomError {
   constructor(message: string = "Service is not initialized") {
     super(message);
     this.name = "ServiceUninitializedError";
   }
 }
 
-export class InternalServerError extends Error {
+export class InternalServerError extends CustomError {
   constructor(message: string = "Internal Server Error") {
     super(message);
     this.name = "InternalServerError";
   }
 }
 
-export class MemoNotFoundError extends Error {
+export class MemoNotFoundError extends CustomError {
   constructor(message: string = "Memo not found") {
     super(message);
     this.name = "MemoNotFoundError";
   }
 }
 
-export class HashMismatchError extends Error {
+export class HashMismatchError extends CustomError {
   constructor(message: string = "Hash mismatch") {
     super(message);
     this.name = "HashMismatchError";
   }
 }
 
-export class MemoDatabaseCorruptedError extends Error {
+export class MemoDatabaseCorruptedError extends CustomError {
   constructor(message: string = "Memo database is corrupted") {
     super(message);
     this.name = "MemoDatabaseCorruptedError";
